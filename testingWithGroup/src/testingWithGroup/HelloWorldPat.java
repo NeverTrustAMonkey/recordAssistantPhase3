8ce4df397f031580cf1a75851d38c96019fe6090
Patrick Sanchez <plsanch@uw.edu>
2016-10-17 10:31:36 -0700

    my print

---
+++ testingWithGroup/src/testingWithGroup/HelloWorldPat.java
@@ -0,0 +1,10 @@
+package testingWithGroup;
+
+public class HelloWorldPat {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello WOrld");
+    }
+
+}
