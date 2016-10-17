9253fb83bd3fdc2b5e1f4038da0814c42b0602e1
Ibrahim Diabate <diabai@uw.edu>
2016-10-17 10:00:25 -0700

    Group test

---
+++ testingWithGroup/src/testingWithGroup/GroupTest.java
@@ -0,0 +1,12 @@
+package testingWithGroup;
+
+public class GroupTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is the first test -- group 1");
+	}
+
+}
