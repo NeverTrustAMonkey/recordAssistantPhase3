94856379854145ac3955ed8f922d962b4d13566e
Ibrahim Diabate <diabai@uw.edu>
2016-11-20 13:52:53 -0800

    last test

--- RecordAssistant/src/internship/Internship.java
+++ RecordAssistant/src/internship/Internship.java
@@ -14,7 +14,7 @@ public class Internship {
 	public void test() {
 		System.out.println("Test ");
 		     
-		System.out.println("Test f");
+		System.out.println("Test know how it works");
 		
 	}
 }
